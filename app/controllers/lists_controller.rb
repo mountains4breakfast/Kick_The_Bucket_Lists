9c1c8b628f59529ffca65e29a42fcc6521384b43
Kaitlin Anderson <kaitlin_anderson@mac.com>
2015-12-11 18:30:07 -0700

    lists_controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,46 @@
 class ListsController < ApplicationController
+before action :find_list, only[:show, :edit, :update]
+
   def index
+    @lists = List.all
   end
 
   def show
+    @items = @list.items
+    @user = User.find(@list.user_id)
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def list_params
+      params.require(:list).permit(:title, :description, :user_id)
+    end
+
+    def find_list
+      @list = List.find(params[:id])
+    end
 end

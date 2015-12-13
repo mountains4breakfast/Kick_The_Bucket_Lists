cb0519ffc9a3fcfca73f99599037a82454deb3eb
Colin Anderson <andercol.4@gmail.com>
2015-12-12 17:57:59 -0700

    joined view

--- app/controllers/lists_items_controller.rb
+++ app/controllers/lists_items_controller.rb
@@ -1,2 +1,7 @@
 class ListsItemsController < ApplicationController
+  def show
+    @list = List.find(params[:list_id])
+    @item = Item.find(params[:item_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :lists
   resources :items
   get 'tags/:tag', to:'items#index', as: :tag
-
+  get '/lists/:list_id/items/:item_id', to:'lists_items#show', as: 'join'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
